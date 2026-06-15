a7fa0e56835bc439627d0262f4f23030270e0fba
Amanda Pereira <amandape17@outlook.com>
2026-06-15 09:53:01 -0300

    Add files via upload

---
+++ Exercicios-Basicos/PromocaoAutomatica.java
@@ -0,0 +1,30 @@
+public class PromocaoAutomatica {
+
+	public static void main(String[] args) {
+
+		byte a = 10;
+		short b = 20;
+		char c = 'A';
+		int d = 30;
+		long e = 40L;
+		float f = 1.5f;
+		double g = 2.5;
+
+		int resultado1 = a + b;
+		long resultado2 = e - d;
+		double resultado3 = f * g;
+
+
+		System.out.println("Resultado da soma (byte + short): " + resultado1);
+		System.out.println("Resultado da subtração (Long - int): " + resultado2);
+		System.out.println("Resultado da multiplicação (float * double): " + resultado3);
+		System.out.println("Valor do char:" + c);
+
+
+	}
+ } 
+
+ 
+		
+
+
