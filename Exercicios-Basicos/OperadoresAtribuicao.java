f8709b1fb79608770cb84516214b6f9065e017e0
Amanda Pereira <amandape17@outlook.com>
2026-06-18 22:41:11 -0300

    Add files via upload

---
+++ Exercicios-Basicos/OperadoresAtribuicao.java
@@ -0,0 +1,16 @@
+public class OperadoresAtribuicao {
+	public static void main(String[] args) {
+		
+		int numero = 5;
+
+		numero += 3; // Equivalente numero = numero + 3;
+		numero -= 2; // Equivalente numero = numero - 2;
+		numero *= 4; // Equivalente numero = numero * 4;
+	    numero /= 2; // Equivalente numero = numero /=2;
+
+	    System.out.println("Valor final: " + numero);
+		
+
+	}
+
+}
